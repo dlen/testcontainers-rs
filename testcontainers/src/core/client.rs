c43eba303f234251d0624a22a8200890f8d18d32
Diego Lendoiro <diego@passbolt.com>
2024-05-09 20:50:05 +0200

    Added method to inspect networks

--- testcontainers/src/core/client.rs
+++ testcontainers/src/core/client.rs
@@ -1,14 +1,16 @@
-use std::{io, time::Duration};
+use std::{default, io, time::Duration};
 
 use bollard::{
     auth::DockerCredentials,
     container::{Config, CreateContainerOptions, LogsOptions, RemoveContainerOptions},
     exec::{CreateExecOptions, StartExecOptions, StartExecResults},
     image::CreateImageOptions,
-    network::CreateNetworkOptions,
+    network::{CreateNetworkOptions, InspectNetworkOptions},
     Docker,
 };
-use bollard_stubs::models::{ContainerCreateResponse, ContainerInspectResponse, HealthStatusEnum};
+use bollard_stubs::models::{
+    ContainerCreateResponse, ContainerInspectResponse, HealthStatusEnum, Network,
+};
 use futures::{StreamExt, TryStreamExt};
 use tokio::sync::OnceCell;
 
@@ -230,6 +232,16 @@ impl Client {
             .unwrap();
 
         network.id
+    }
+
+    /// Inspects a network
+    pub(crate) async fn inspect_network(
+        &self,
+        name: &str,
+    ) -> Result<Network, bollard::errors::Error> {
+        self.bollard
+            .inspect_network(name, Some(InspectNetworkOptions::<String>::default()))
+            .await
     }
 
     pub(crate) async fn create_container(
